b8e6ee0732573dc68007dccd95c1de174eb757d9
Vivek Ramesh <vivz@Vivz.local>
2015-05-14 14:50:57 +0530

    This is my first commit

---
+++ HelloWorld/Person.swift
@@ -0,0 +1,16 @@
+//
+//  Person.swift
+//  HelloWorld
+//
+//  Created by Vivek Ramesh on 12/05/15.
+//  Copyright (c) 2015 slidenerd. All rights reserved.
+//
+
+import Foundation
+class Person
+{
+    var firstName : String;
+    init(firstName : String){
+        self.firstName = firstName;
+    }
+}
--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,13 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var x = 90;
     override func viewDidLoad() {
         super.viewDidLoad()
+        test();
+        x = 100;
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func test(){
+        var a = 900
+        println("So did you see the breakpoint, did you?")
+        a = 1000
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        x = 200;
         // Dispose of any resources that can be recreated.
     }
 
